81c2a87e5800728e5d57164305ffac10984a6723
Ahmed hossamaldin hamdy <ahmedhoosam2021@gmail.com>
2025-12-13 22:23:23 +0200

    final version

--- Tests.fs
+++ Tests.fs
@@ -1,56 +1,37 @@
-﻿module Tests
+module Tests
 
 open System
 open wordmodel
 open Operations
 
-
 let private assertSuccess operationName oldDict result =
     match result with
     | Ok newDict -> 
-        printfn " %s: Passed" operationName
+        printfn "%s: Success." operationName
         newDict
     | Error e -> 
-        printfn " %s: Failed -> %A" operationName e
+        printfn "%s: Failed. Error details: %A" operationName e
         oldDict 
 
 let runTests () =
-    printfn "\n🚀 Starting Unit Tests...\n"
-
+    printfn "\nStarting unit tests...\n"
 
     let dict0 = Map.empty
 
-
     let dict1 = 
         addWord "Apple" "A red fruit" dict0 
-        |> assertSuccess "Add 'Apple'" dict0
-
-
-    match fullsearch "APPLE" dict1 with
-    | Some (k, v) -> printfn "SUCCESS Search 'APPLE': Found (%s -> %s)" k v
-    | None        -> printfn "FAILED Search 'APPLE': Not Found"
-
-
-    let partialRes = partialsearch "ppl" dict1
-    if partialRes.ContainsKey "apple" then 
-        printfn "SUCCESS Partial Search 'ppl': Found 'apple'"
-    else 
-        printfn "FAILED Partial Search 'ppl': Failed"
-
-
+        |> assertSuccess "Adding new word 'Apple'" dict0
 
     let dict2 = 
         updateWord "Apple" "Green fruit" dict1 
-        |> assertSuccess "Update 'Apple'" dict1
-
+        |> assertSuccess "Updating meaning of 'Apple'" dict1
 
     let dict3 = 
         deleteWord "Apple" dict2 
-        |> assertSuccess "Delete 'Apple'" dict2
-
+        |> assertSuccess "Deleting word 'Apple'" dict2
 
     match fullsearch "apple" dict3 with
-    | None   -> printfn "SUCCESS Verification: Word is truly gone."
-    | Some _ -> printfn "FAILED Verification: Word still exists!"
+    | None    -> printfn "Verification: The word was successfully removed."
+    | Some _ -> printfn "Verification: Error, the word still exists."
 
-    printfn "\n Tests Finished.\n"
+    printfn "\nTests completed.\n"
